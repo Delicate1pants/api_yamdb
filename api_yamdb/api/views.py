ea5b23163ae5d381f1c9a655d4c84d939f289f0d
Georgiy <Patrimonio29@ya.ru>
2021-11-03 17:43:10 +0700

    code is done, but no tests yet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from .serializers import CommentSerializer, ReviewSerializer
+from reviews.models import Comment, Review, Title
+
+
+class CustomModelViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class ReviewViewSet(CustomModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (AuthorOrReadOnly)
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 3
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        get_object_or_404(Title, pk=title_id)
+        new_queryset = Review.objects.filter(title=title_id)
+        return new_queryset
+
+
+class CommentViewSet(CustomModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly)
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 5
+
+    def get_queryset(self):
+        review_id = self.kwargs.get("review_id")
+        get_object_or_404(Review, pk=review_id)
+        new_queryset = Comment.objects.filter(review=review_id)
+        return new_queryset
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    score = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)])
+    text = models.TextField()
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique review'
+            )
+        ]
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        auto_now_add=True)
