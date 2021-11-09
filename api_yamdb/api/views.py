1a7317161308f528630ef20e90d3df31741d2d08
Delicate1pants <86769719+Delicate1pants@users.noreply.github.com>
2021-11-09 11:32:38 +0700

    manual merge reviews_control + titles_control

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
 
-from .serializers import CommentSerializer, ReviewSerializer
-from reviews.models import Comment, Review, Title
+from .serializers import (CategorySerializer, CommentSerializer,
+                          GenreSerializer, ReviewSerializer,
+                          TitlesReadSerializer, TitlesWriteSerializer)
+from reviews.models import Category, Comment, Genre, Review, Titles
 
 
 class CustomModelViewSet(viewsets.ModelViewSet):
@@ -13,24 +16,56 @@ class CustomModelViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
+class CategoryViewSet(
+    mixins.ListModelMixin, mixins.CreateModelMixin,
+    mixins.DestroyModelMixin, viewsets.GenericViewSet
+):
+    queryset = Category.objects.all()
+    lookup_field = "slug"
+    serializer_class = CategorySerializer
+    # permission_classes = []
+    filter_backends = [SearchFilter]
+    search_fields = ("name",)
+
+
+class GenreViewSet(
+    mixins.ListModelMixin, mixins.CreateModelMixin,
+    mixins.DestroyModelMixin, viewsets.GenericViewSet
+):
+    queryset = Genre.objects.all()
+    lookup_field = 'slug'
+    serializer_class = GenreSerializer
+    # permission_classes = []
+    filter_backends = [SearchFilter]
+    search_fields = ['=name', ]
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Titles.objects.all()
+    # permission_classes = []
+
+    def get_serializer_class(self):
+        if self.request.method in ['list', 'retrieve']:
+            return TitlesReadSerializer
+        return TitlesWriteSerializer
+
+
 class ReviewViewSet(CustomModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = (AuthorOrReadOnly)
+    # permission_classes = (AuthorOrReadOnly)
     pagination_class = PageNumberPagination
-    pagination_class.page_size = 3
 
     def get_queryset(self):
         title_id = self.kwargs.get("title_id")
-        get_object_or_404(Title, pk=title_id)
+        get_object_or_404(Titles, pk=title_id)
         new_queryset = Review.objects.filter(title=title_id)
         return new_queryset
 
 
 class CommentViewSet(CustomModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly)
+    # permission_classes = (AuthorOrReadOnly)
     pagination_class = PageNumberPagination
-    pagination_class.page_size = 5
 
     def get_queryset(self):
         review_id = self.kwargs.get("review_id")
